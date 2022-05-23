a5f3b6890663bc540c7cf542395081cd1db50108
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-05-23 14:23:37 +0200

    admin asset loader function to admin layout

--- resources/views/admin_layout/left_sidebar.blade.php
+++ resources/views/admin_layout/left_sidebar.blade.php
@@ -5,7 +5,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="index3.html" class="brand-link">
-      <img src="backend/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">BujaMart - Admin</span>
   </a>
 
@@ -14,7 +14,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-              <img src="backend/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+              <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
               <a href="#" class="d-block">Alex Irakoze</a>
