88b2c5320d4f944f9d692ae991f225315159d72c
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-09 12:19:15 +0200

    Created the addtocart method

--- app/Cart.php
+++ app/Cart.php
@@ -16,7 +16,36 @@
     }
 
     public function add($item, $product_id) {
+      // Initialize the content of a cart item
+      $storedItem = [
+        'qty' => 0, 
+        'product_id' => 0, 
+        'product_name' => $item->product_name, 
+        'product_price' => $item->product_price, 
+        'product_description' => $item->product_description, 
+        'product_image' => $item->product_image, 
+        'item' => $item
+      ];
+
+      // check if the product already exist in cart
+      if($this->items) {
+        if(array_key_exists($product_id, $this->items)) {
+          $storedItem = $this->items($product_id);
+        }
+      }
+
+      // Add the product to cart or increase qty, totalQty and Total Price
+      $storedItem['qty']++;
+      $storedItem['product_id'] = $product_id;
+      $storedItem['product_name'] = $item->product_name;
+      $storedItem['product_price'] = $item->product_price;
+      $storedItem['product_description'] = $item->product_description;
+      $storedItem['product_image'] = $item->product_image;
       
+      
+      $this->totalQty++;
+      $this->totalPrice += $item->product_price;
+      $this->items[$product_id] = $storedItem;
     }
   }
 ?>
