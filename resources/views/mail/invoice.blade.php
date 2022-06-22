2bb9bd03c699213bb1959d340e8233b962b0c20e
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-22 09:11:37 +0200

    Made some changes on the invoice

--- resources/views/mail/invoice.blade.php
+++ resources/views/mail/invoice.blade.php
@@ -3,9 +3,7 @@
   <head>
     <title>Confirmation de Commande - BujaMart</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	  <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
-	  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="frontend/css/style.css">
     <style  type="text/css">
       body{margin-top:20px;
             color: #2e323c;
@@ -273,7 +271,7 @@
                     <!-- Row end -->
                   </div>
                   <div class="invoice-footer">
-                    Thank you for your Business.
+                    Merci de choisir BujaMart
                   </div>
                 </div>
               </div>
@@ -281,5 +279,9 @@
           </div>
         </div>
       </div>
+      <script src="frontend/js/jquery.min.js"></script>
+      <script src="frontend/js/jquery-migrate-3.0.1.min.js"></script>
+      <script src="frontend/js/popper.min.js"></script>
+      <script src="frontend/js/bootstrap.min.js"></script>
   </body>
 </html>
