e1a7b1ec7e5da63f0d505a370fc988e4b9803224
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-06-23 21:23:47 +0200

    Nathing changed even while using the Bootstrap CDN

--- resources/views/mail/invoice.blade.php
+++ resources/views/mail/invoice.blade.php
@@ -3,7 +3,7 @@
   <head>
     <title>Confirmation de Commande - BujaMart</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="frontend/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <style  type="text/css">
       body{margin-top:20px;
             color: #2e323c;
@@ -279,9 +279,8 @@
           </div>
         </div>
       </div>
-      <script src="frontend/js/jquery.min.js"></script>
-      <script src="frontend/js/jquery-migrate-3.0.1.min.js"></script>
-      <script src="frontend/js/popper.min.js"></script>
-      <script src="frontend/js/bootstrap.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
   </body>
 </html>
